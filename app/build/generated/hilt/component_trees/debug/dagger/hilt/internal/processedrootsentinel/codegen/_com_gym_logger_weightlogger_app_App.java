2c0fa9c934277033105c78001e71b6ca9b168181
pravin desai <pravindesai100@gmail.com>
2023-01-28 22:44:29 +0530

    init

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_gym_logger_weightlogger_app_App.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.gym.logger.weightlogger.app.App"
-)
-public final class _com_gym_logger_weightlogger_app_App {
-}
